88083af1b5b4694bfc521dcf3624f654da242a06
jjaegii <hn06038@gmail.com>
2022-12-28 17:33:34 +0900

    change to use convert module

--- draw_sound.py
+++ draw_sound.py
@@ -2,16 +2,9 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import os
+from src import convert
 
-audio_folder = 'sounds/'
+audio_folder = 'sample'
 
 for i in range(len(os.listdir(audio_folder))):
-    # y = 파형의 amplitude 값
-    # sr = sampling rate
-    y, sr = librosa.load(audio_folder + str(i+1) + '.wav')
-
-    plt.figure()
-    librosa.display.waveshow(y, sr=sr)
-    # plt.show()
-
-    plt.savefig(audio_folder + str(i+1) + '.png')
+    convert.run(str(i+1) + '.wav', audio_folder)
