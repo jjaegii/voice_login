de65bd76dd27837eea67467fdbbdf9c79bc02855
jjaegii <hn06038@gmail.com>
2023-01-02 15:08:11 +0900

    add mfcc function

--- src/convert.py
+++ src/convert.py
@@ -1,4 +1,4 @@
-''' wav 파일을 이미지화 시키는 모듈 '''
+''' 오디오 파일을 이미지화 시키는 모듈 '''
 import os
 import librosa
 import librosa.display
@@ -11,9 +11,10 @@ SOUND_FOLDER = None
 # 2D 음파 그래프
 def convert2D(wavfile, y, sr):
     plt.figure()
+    plt.axis('off')
     librosa.display.waveshow(y, sr=sr)
     img_path = os.path.join(SOUND_FOLDER, wavfile.split('.')[0] + '_2d.png')
-    plt.savefig(img_path)
+    plt.savefig(img_path, bbox_inches='tight')
     return img_path
 
 # 푸리에 변환
@@ -22,7 +23,7 @@ def convertFourier(wavfile, y):
     plt.figure()
     plt.plot(D)
     img_path = os.path.join(SOUND_FOLDER, wavfile.split('.')[0] + '_fourier.png')
-    plt.savefig(img_path)
+    plt.savefig(img_path, bbox_inches='tight')
     return img_path
 
 # 2D, 푸리에 합친 후 삭제
@@ -40,11 +41,34 @@ def merge(wavfile, path_2d, path_fourier):
     os.remove(path_2d)
     os.remove(path_fourier)
     os.remove(os.path.join(SOUND_FOLDER, wavfile))
+    return img_path
+
+# MFCCs 변환 후 삭제
+def mfcc(wavfile, y, sr):
+    FIG_SIZE = (15, 10)
+    hop_length = 512
+    n_fft = 2048
+
+    MFCCs = librosa.feature.mfcc(y, sr, n_fft=n_fft, hop_length=hop_length, n_mfcc=13)
+    plt.figure()
+    plt.axis('off')
+    librosa.display.specshow(MFCCs, sr=sr, hop_length=hop_length)
+    img_folder = os.path.join(SOUND_FOLDER, wavfile.split('.')[0])
+    if not os.path.exists(img_folder):
+        os.mkdir(img_folder)
+    img_path = os.path.join(img_folder, 'mfcc.png')
+    plt.savefig(img_path, bbox_inches='tight')
+    os.remove(os.path.join(SOUND_FOLDER, wavfile))
+    return img_path
+    
 
 def run(wavfile, path):
+    # 소리가 없는 부분 자르는 기능이 필요함
     global SOUND_FOLDER
     SOUND_FOLDER = path
     y, sr = librosa.load(os.path.join(SOUND_FOLDER, wavfile))
-    path_2d = convert2D(wavfile, y, sr)
-    path_fourier = convertFourier(wavfile, y)
-    merge(wavfile, path_2d, path_fourier)
+    # path_2d = convert2D(wavfile, y, sr)
+    # path_fourier = convertFourier(wavfile, y)
+    # img_path = merge(wavfile, path_2d, path_fourier)
+    img_path = mfcc(wavfile, y, sr)
+    return img_path
